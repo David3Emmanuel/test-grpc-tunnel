3c32a2b4a09a7ccca30acf58303cea4eddc02b18
David3Emmanuel <david3emmanuel@gmail.com>
2025-07-17 15:02:15 +0100

    Connect to cloud server

--- client.py
+++ client.py
@@ -3,8 +3,13 @@ import grpc
 from hello_world_pb2_grpc import GreeterStub
 from hello_world_pb2 import HelloRequest, HelloResponse
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-with grpc.insecure_channel("localhost:50051") as channel:
+SERVER_URL = os.getenv("SERVER_URL", "localhost:50051")
+
+with grpc.insecure_channel(SERVER_URL) as channel:
     stub = GreeterStub(channel)
     request = HelloRequest(name="David")
     response: HelloResponse = stub.SayHello(request)
